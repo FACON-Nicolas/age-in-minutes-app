5af187ee52dc5c4e1af40089c45a7fc097355e17
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-01-26 09:53:57 +0000

    add app

---
+++ app/src/main/java/faconnicolas/project/ageinminutes/MainActivity.kt
@@ -0,0 +1,61 @@
+package faconnicolas.project.ageinminutes
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.lang.Math.abs
+import java.text.SimpleDateFormat
+import java.util.*
+
+// coolors.co
+class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate: TextView? = null
+    private var tvMinutes: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvMinutes = findViewById(R.id.tvMinutes)
+
+        btnDatePicker.setOnClickListener { view ->
+            clickDatePicker()
+        }
+    }
+
+    private fun clickDatePicker() {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(this, { view, y, m, d ->
+            val selectedDate = "$d/${m+1}/$y"
+            tvSelectedDate?.text = selectedDate
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE)
+            val theDate = sdf.parse(selectedDate)
+            theDate?.let {
+                val selectedDateInMinutes = theDate.time / 60000
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                currentDate?.let {
+                    val currentDateInMinutes = currentDate.time / 60000
+                    val minutes = abs(currentDateInMinutes - selectedDateInMinutes)
+                    tvMinutes?.text = minutes.toString()
+                }
+            }
+            },
+            year,
+            month,
+            day
+        )
+
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
+    }
+}
